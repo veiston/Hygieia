83e2d3efe5e804fc78fae3ee2511d20fe195f310
veiston <veikka.liukkonen@gmail.com>
2025-04-11 23:34:05 +0300

    +Bat +Sh

--- Hygieia.bat
+++ Hygieia.bat
@@ -1,5 +1,9 @@
 @echo off
-
-cd C:\Users\veikk\Documents\GitHub\Hygieia
+pushd "C:\Users\veikk\Documents\GitHub\Hygieia" || (
+	echo Failed to change directory.
+	pause
+	exit /b 1
+)
 py Hygieia_AI.py
+popd
 pause
---
+++ Hygieia.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"
+python3 Hygieia_AI.py
